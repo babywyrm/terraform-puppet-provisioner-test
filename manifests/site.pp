925687b4863ea019323fb5323679009420afbefd
feanor <tms@TMSs-MacBook-Air.local>
2020-03-09 21:22:56 -0500

    Just to reiterate- Remove windows agent for now, Updated docs, Updated base site manifest, super basics, What's next?  Destroy and start again tomorrow. Bye.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,16 @@
 node default {
   notify { "Hello world from ${facts['hostname']}!": }
 }
+
+node 'puppet.agent' { # Applies only to mentioned node; if nothing mentioned, applies to all.
+file { '/tmp/PUPPETDEATH_LOL': # Resource type file
+
+## ensure => 'directory', # Create as a directory
+ ensure => 'present', # Make sure it exists
+ owner => 'root', # Ownership
+ group => 'root', # Group Name
+ mode => '0755', # Directory permissions
+ content => "This File is created by the Puppet Master, Obvi"
+
+}
+}
